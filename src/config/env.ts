c8e42b2d083982f5a4568000a0ddc341a2fdd7cc
Romerito Queiroz <77184434+hugoromerito@users.noreply.github.com>
2025-09-01 19:56:44 +0000

    Configura variáveis ambiente

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,9 +3,6 @@ import { z } from 'zod/v4'
 
 export const env = createEnv({
   server: {
-    PORT: z.coerce.number().default(3333),
-    DATABASE_URL: z.url().startsWith('postgresql://'),
-    JWT_SECRET: z.string(),
     GOOGLE_OAUTH_CLIENT_ID: z
       .string(),
     GOOGLE_OAUTH_CLIENT_SECRET: z
@@ -21,9 +18,6 @@ export const env = createEnv({
       .url(),
   },
   runtimeEnv: {
-    PORT: process.env.PORT,
-    DATABASE_URL: process.env.DATABASE_URL,
-    JWT_SECRET: process.env.JWT_SECRET,
     GOOGLE_OAUTH_CLIENT_ID: process.env.GOOGLE_OAUTH_CLIENT_ID,
     GOOGLE_OAUTH_CLIENT_SECRET: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
     GOOGLE_OAUTH_CLIENT_REDIRECT_URI:
