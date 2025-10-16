6d8fbd698c7641ac2619047ea4ee86bf2084f639
Romerito Queiroz <77184434+hugoromerito@users.noreply.github.com>
2025-10-16 22:05:17 +0000

    feat: add HTTP barrel export
    - Add centralized exports for all HTTP functions

---
+++ src/http/index.ts
@@ -0,0 +1,61 @@
+// Auth
+export * from './sign-in-with-password'
+export * from './sign-in-with-google'
+export * from './sign-up'
+export * from './get-profile'
+export * from './request-password-recover'
+export * from './reset-password'
+
+// Organizations
+export * from './create-organization'
+export * from './get-organization'
+export * from './get-organizations'
+export * from './get-membership'
+export * from './update-organization'
+export * from './shutdown-organization'
+
+// Units
+export * from './create-unit'
+export * from './get-units'
+
+// Users
+export * from './create-user'
+export * from './get-users'
+
+// Members
+export * from './get-members'
+export * from './get-members-organization'
+export * from './get-members-unit'
+
+// Invites
+export * from './accept-invite'
+export * from './reject-invite'
+export * from './get-invite'
+export * from './get-invites'
+export * from './get-pending-invites'
+export * from './get-organization-invites'
+export * from './create-invite'
+
+// Applicants
+export * from './create-applicant'
+export * from './get-applicant'
+export * from './get-check-applicant-slug'
+export * from './get-applicant-demands'
+
+// Demands
+export * from './create-demand'
+export * from './get-demand'
+export * from './get-demands'
+export * from './update-demand'
+export * from './update-demand-status' // Deprecated
+
+// Attachments
+export * from './upload-user-avatar'
+export * from './upload-applicant-avatar'
+export * from './upload-organization-avatar'
+export * from './upload-applicant-document'
+export * from './upload-demand-document'
+export * from './upload-organization-document'
+export * from './get-attachments'
+export * from './download-attachment'
+export * from './delete-attachment'
